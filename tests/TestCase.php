9c757ab3db1ec111e5468b68eb78bfd550cb09d6
Mark van den Broek <mvdnbrk@gmail.com>
2018-06-05 18:50:11 +0000

    Apply fixes from StyleCI

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,7 +9,7 @@ abstract class TestCase extends BaseTestCase
     /**
      * Get package providers.
      *
-     * @param  \Illuminate\Foundation\Application  $app
+     * @param \Illuminate\Foundation\Application $app
      *
      * @return array
      */
