13dba8e3bc5a5f5466daf1401f096e5b88d8b460
Mark van den Broek <mvdnbrk@gmail.com>
2019-06-03 08:00:46 +0200

    Fixes docblock.

--- src/helpers.php
+++ src/helpers.php
@@ -6,7 +6,7 @@ if (! function_exists('is_gtin')) {
     /**
      * Determine if a value is valid Global Trade Identification Number.
      *
-     * @param  string  $value
+     * @param  string|int  $value
      * @return bool
      */
     function is_gtin($value)
