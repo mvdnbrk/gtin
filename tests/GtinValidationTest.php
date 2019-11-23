8ecb7ada1e3423e5a01b275d8bddead507b9214d
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-23 16:10:34 +0100

    Refactor tests.

--- tests/GtinValidationTest.php
+++ tests/GtinValidationTest.php
@@ -2,189 +2,208 @@
 
 namespace Mvdnbrk\Gtin\Tests;
 
+use Illuminate\Support\Facades\Validator;
+
 class GtinValidationTest extends TestCase
 {
-    /**
-     * @var \Illuminate\Validation\Factory
-     */
-    protected $validator;
-
-    /**
-     * Setup the test environment.
-     *
-     * @return void
-     */
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        $this->validator = $this->app['validator'];
-    }
-
     /** @test */
     public function a_valid_gtin8_should_pass()
     {
-        $this->assertTrue($this->validator->make(
+        $validator = Validator::make(
             ['field' => '80000006'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertTrue($validator->passes());
     }
 
     /** @test */
     public function an_invalid_gtin8_should_not_pass()
     {
-        $this->assertFalse($this->validator->make(
+        $validator = Validator::make(
             ['field' => '80000000'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertFalse($validator->passes());
     }
 
     /** @test */
     public function a_valid_gtin12_should_pass()
     {
-        $this->assertTrue($this->validator->make(
+        $validator = Validator::make(
             ['field' => '120000000005'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertTrue($validator->passes());
     }
 
     /** @test */
     public function an_invalid_gtin12_should_not_pass()
     {
-        $this->assertFalse($this->validator->make(
+        $validator = Validator::make(
             ['field' => '120000000000'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertFalse($validator->passes());
     }
 
     /** @test */
     public function a_valid_gtin13_should_pass()
     {
-        $this->assertTrue($this->validator->make(
+        $validator = Validator::make(
             ['field' => '1300000000000'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertTrue($validator->passes());
     }
 
     /** @test */
     public function an_invalid_gtin13_should_not_pass()
     {
-        $this->assertFalse($this->validator->make(
+        $validator = Validator::make(
             ['field' => '1300000000001'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertFalse($validator->passes());
     }
 
     /** @test */
     public function a_valid_gtin14_should_pass()
     {
-        $this->assertTrue($this->validator->make(
+        $validator = Validator::make(
             ['field' => '14000000000003'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertTrue($validator->passes());
     }
 
     /** @test */
     public function an_invalid_gtin14_should_not_pass()
     {
-        $this->assertFalse($this->validator->make(
+        $validator = Validator::make(
             ['field' => '14000000000000'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertFalse($validator->passes());
     }
 
     /** @test */
     public function a_valid_gtin_integer_value_should_pass()
     {
-        $this->assertTrue($this->validator->make(
+        $validator = Validator::make(
             ['field' => 80000006],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertTrue($validator->passes());
     }
 
     /** @test */
     public function a_too_short_value_should_not_pass()
     {
-        $this->assertFalse($this->validator->make(
+        $validator = Validator::make(
             ['field' => '7000003'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertFalse($validator->passes());
     }
 
     /** @test */
     public function a_too_long_value_should_not_pass()
     {
-        $this->assertFalse($this->validator->make(
+        $validator = Validator::make(
             ['field' => '150000000000004'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertFalse($validator->passes());
     }
 
     /** @test */
     public function a_value_with_a_length_of_nine_digits_should_not_pass()
     {
-        $this->assertFalse($this->validator->make(
+        $validator = Validator::make(
             ['field' => '900000001'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertFalse($validator->passes());
     }
 
     /** @test */
     public function a_value_with_a_length_of_ten_digits_should_not_pass()
     {
-        $this->assertFalse($this->validator->make(
+        $validator = Validator::make(
             ['field' => '1000000007'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertFalse($validator->passes());
     }
 
     /** @test */
     public function a_value_with_a_length_of_eleven_digits_should_not_pass()
     {
-        $this->assertFalse($this->validator->make(
+        $validator = Validator::make(
             ['field' => '11000000006'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertFalse($validator->passes());
     }
 
     /** @test */
     public function zeros_should_not_pass()
     {
-        $this->assertFalse($this->validator->make(
+        $validator = Validator::make(
             ['field' => '0000000000000'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertFalse($validator->passes());
     }
 
     /** @test */
     public function a_string_should_not_pass()
     {
-        $this->assertFalse($this->validator->make(
+        $validator = Validator::make(
             ['field' => 'string'],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertFalse($validator->passes());
     }
 
     /** @test */
     public function an_array_should_not_pass()
     {
-        $this->assertFalse($this->validator->make(
+        $validator = Validator::make(
             ['field' => []],
             ['field' => 'gtin']
-        )->passes());
+        );
+
+        $this->assertFalse($validator->passes());
     }
 
     /** @test */
     public function it_returns_the_correct_error_message()
     {
-        $validation = $this->validator->make(
+        $validator = Validator::make(
             ['field' => 'invalid-gtin'],
             ['field' => 'gtin']
         );
 
         $this->assertEquals(
             'The field must be a valid Global Trade Item Number (GTIN).',
-            $validation->errors()->first('field')
+            $validator->errors()->first('field')
         );
     }
 
